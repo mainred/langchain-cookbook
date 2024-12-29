df6da47c27c0533a78638999b89ba98084ff12df
Qingchuan Hao <qingchuan.hao@microsoft.com>
2024-12-29 15:15:56 +0000

    fix no match error for zsh

--- shell-ai.py
+++ shell-ai.py
@@ -3,6 +3,10 @@
 # - train a fine-tuned model from `shell-ai-traning-data.jsonl` on Azure OpenAI Service and create a deployment out of it
 # - specify the deployment and auth info in the .env
 # - add command_not_found_handler to capture command not found issue, and execute the ai script to give answers
+
+# disable nomatch
+# zsh tries to expand the wildcard for glob, and warns you when no match. This will prevent us from adding a ? at the tail of our questions
+# unsetopt nomatch
 # command_not_found_handler() {
 #     python /home/azureuser/llm/langchain/langchain-cookbook/shell-ai.py "$@"
 #     if [ $? = 0 ]; then
@@ -23,9 +27,11 @@ from langchain_openai import AzureChatOpenAI
 model = AzureChatOpenAI(
     openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
     azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+    openai_api_type=os.environ["AZURE_OPENAI_API_VERSION"],
 )
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     sys.exit(1)
-question = sys.argv[1]
+
+question = " ".join(sys.argv[1:])
 print(model.invoke(question).content)
